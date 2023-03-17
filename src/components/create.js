f926e4c08878d9e392c6bfb131fb6b025be5a078
januspaul <januspaul52@gmail.com>
2023-03-18 04:50:37 +0800

    e

---
+++ src/components/create.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { Button, Checkbox, Form } from 'semantic-ui-react';
+import axios from 'axios';
+
+export default function Create() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [checkbox, setCheckbox] = useState(false);
+  const postData = () => {
+    axios.post(`https://6414bb8ae8fe5a3f3a0e7d07.mockapi.io/Users`, {
+        firstName,
+        lastName,
+        checkbox
+    })
+}
+  return (
+    <div>
+      <Form className="create-form">
+        <Form.Field>
+          <label>First Name</label>
+          <input
+            placeholder="First Name"
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Last Name</label>
+          <input
+            placeholder="Last Name"
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </Form.Field>
+        <Button onClick={postData} type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+}
